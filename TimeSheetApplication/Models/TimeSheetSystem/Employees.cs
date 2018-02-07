6a3faf144f2b49bdc8f8684a4f0763ac93833c42
Chloee Robertson <me.c.leigh@gmail.com>
2018-02-07 11:50:20 -0800

    Added Employees class, and LaborGrades class. I have also added a migrations.txt file so that I can link to the db

---
+++ TimeSheetApplication/Models/TimeSheetSystem/Employees.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeSheetApplication.Models.TimeSheetSystem
+{
+    public class Employees
+    {
+        public string EmployeeNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public LaborGrades LaborGrade { get; set; }
+        public string Grade { get; set; }
+
+        public string EmployeeIntials { get; set; }
+        public Employees Supervisor { get; set; }
+
+        public string SupervisorNumber { get; set; }
+        //TODO: Determine if there should be a column to indicate if the individual can be a supervisor
+        //public AuthorizationCodes AuthCode { get; set; }
+    }
+}
---
+++ TimeSheetApplication/Models/TimeSheetSystem/LaborGrades.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeSheetApplication.Models.TimeSheetSystem
+{
+    public class LaborGrades
+    {
+        public string Grade { get; set; }
+        public double PayAmount { get; set; }
+    }
+}
