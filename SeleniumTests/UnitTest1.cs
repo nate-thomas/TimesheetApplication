3a5ec693e14dd61c10c7649bd46889375880fa45
John <johnnjpark@gmail.com>
2018-02-13 16:02:27 -0800

    initial commit for selenium tests

---
+++ SeleniumTests/UnitTest1.cs
@@ -0,0 +1,75 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Reflection;
+
+namespace SeleniumTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SampleTest()
+        {
+
+            var driverDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            IWebDriver driver = new ChromeDriver(driverDir);
+
+
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("How to cook");
+            driver.FindElement(By.Name("btnK")).Submit();
+            
+
+            Assert.IsNotNull(driver.FindElement(By.XPath("//div[@id='resultStats']")));
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void TestLogin()
+        {
+
+            var driverDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            IWebDriver driver = new ChromeDriver(driverDir);
+
+            driver.Navigate().GoToUrl("https://lab03-v2.azurewebsites.net");
+            driver.FindElement(By.XPath("//*[@id='registerLink']")).Click();
+            driver.FindElement(By.XPath("//*[@id='Email']")).SendKeys("aasddd@ad.af");
+
+            driver.FindElement(By.XPath("//*[@id='Password']")).SendKeys("123qwe!@#QWE");
+            driver.FindElement(By.XPath("//*[@id='ConfirmPassword']")).SendKeys("123qwe!@#QWE");
+            driver.FindElement(By.XPath("//*[@value='Register']")).Submit();
+
+            var check = driver.FindElement(By.XPath("//*[@title='Manage']"));
+
+            Assert.AreEqual(check.Text, "Hello aasddd@ad.af!");
+
+
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void CounterTest()
+        {
+
+            var driverDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            IWebDriver driver = new ChromeDriver(driverDir);
+
+            driver.Navigate().GoToUrl("https://timesheetapplication.azurewebsites.net/counter");
+
+            var check = driver.FindElement(By.XPath("//strong[text()='0']"));
+            for (var x = 1; x <= 5; x++)
+            {
+                driver.FindElement(By.XPath("//button[text()='Increment']")).Click();
+
+            }
+
+            
+
+            Assert.AreEqual(check.Text, "5");
+
+
+            driver.Close();
+        }
+    }
+}
