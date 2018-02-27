d5365d4d3d3bae21fee40341e5be77dfffff1fed
Shelze <makaxsole@gmail.com>
2018-02-27 13:53:42 -0800

    added timesheetsTable

---
+++ TimeSheetApplication/ClientApp/app/components/timesheetsTable/timesheetsTable.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+	selector: 'timesheetsTable',
+	templateUrl: './timesheetsTable.component.html',
+	styleUrls: ['./timesheetsTable.component.css']
+
+})
+export class TimesheetsTableComponent {
+}
