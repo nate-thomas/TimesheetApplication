593f5f3603248f062ebf4d1338fe43622cca9187
kenZino <kennethli126@gmail.com>
2018-02-27 14:28:27 -0800

    Added fail test

--- XUnitTestProject/UnitTest1.cs
+++ XUnitTestProject/UnitTest1.cs
@@ -14,14 +14,14 @@ namespace XUnitTestProject1
             Assert.Equal(48, Add(val1, val2));
         }
 
-        //[Fact]
-        //public void FailTest()
-        //{
-        //    int x = 3000;
-        //    int y = 6001;
+        [Fact]
+        public void FailTest()
+        {
+            int x = 3000;
+            int y = 6001;
 
-        //    Assert.Equal(9000, Add(x, y));
-        //}
+            Assert.Equal(9000, Add(x, y));
+        }
 
         int Add(int num1, int num2)
         {
