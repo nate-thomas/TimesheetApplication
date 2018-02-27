217af7110912339c1801d230cd55a888e106ac2f
DESKTOP_WIN10\Victor Starzynski <vnogstar@gmail.com>
2018-02-27 11:14:56 -0800

    GetRequestFor Employees -> EmployeesAPIControlle

---
+++ TimeSheetApplication/Controllers/EmployeesAPIController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimeSheetApplication.Data;
+using TimeSheetApplication.Models.TimeSheetSystem;
+
+namespace TimeSheetApplication.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/EmployeesAPI")]
+    public class EmployeesAPIController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public EmployeesAPIController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Employees> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+    }
+}
--- TimeSheetApplication/Startup.cs
+++ TimeSheetApplication/Startup.cs
@@ -23,8 +23,7 @@ namespace TimeSheetApplication
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
         }
 
