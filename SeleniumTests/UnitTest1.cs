171fcd76310a4c89f0523625867b5704a4e2b5ae
John <johnnjpark@gmail.com>
2018-03-06 11:16:48 -0800

    removing unnecessary failing tests

--- SeleniumTests/UnitTest1.cs
+++ SeleniumTests/UnitTest1.cs
@@ -19,7 +19,7 @@ namespace SeleniumTests
             //chromeOptions.AddArgument("--headless");
             //IWebDriver driver = new ChromeDriver(chromeOptions);
 
-            //Original way to get driver and works locally
+            //***Original way to get driver and works locally***
             var driverDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             IWebDriver driver = new ChromeDriver(driverDir);
 
@@ -30,52 +30,6 @@ namespace SeleniumTests
             Assert.IsNotNull(driver.FindElement(By.XPath("//div[@id='resultStats']")));
             driver.Close();
         }
-
-        //[TestMethod]
-        //public void TestRegister()
-        //{
-
-        //    var driverDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        //    IWebDriver driver = new ChromeDriver(driverDir);
-
-        //    driver.Navigate().GoToUrl("https://lab03-v2.azurewebsites.net");
-        //    driver.FindElement(By.XPath("//*[@id='registerLink']")).Click();
-        //    driver.FindElement(By.XPath("//*[@id='Email']")).SendKeys("qweaasddd@ad.af");
-
-        //    driver.FindElement(By.XPath("//*[@id='Password']")).SendKeys("123qwe!@#QWE");
-        //    driver.FindElement(By.XPath("//*[@id='ConfirmPassword']")).SendKeys("123qwe!@#QWE");
-        //    driver.FindElement(By.XPath("//*[@value='Register']")).Submit();
-
-        //    var check = driver.FindElement(By.XPath("//*[@title='Manage']"));
-
-        //    Assert.AreEqual(check.Text, "Hello qweaasddd@ad.af!");
-
-
-        //    driver.Close();
-        //}
-
-        [TestMethod]
-        public void CounterTest()
-        {
-
-            var driverDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            IWebDriver driver = new ChromeDriver(driverDir);
-
-            driver.Navigate().GoToUrl("https://timesheetapplication.azurewebsites.net/counter");
-
-            var check = driver.FindElement(By.XPath("//strong[text()='0']"));
-            for (var x = 1; x <= 5; x++)
-            {
-                driver.FindElement(By.XPath("//button[text()='Increment']")).Click();
-
-            }
-
-            
-
-            Assert.AreEqual(check.Text, "5");
-
-
-            driver.Close();
-        }
+     
     }
 }
