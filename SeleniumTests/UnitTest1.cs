286fd23b9564b6765560f3378bbf0ee7b7a7daa5
John <johnnjpark@gmail.com>
2018-03-06 11:11:29 -0800

    checking if grabbing web driver works

--- SeleniumTests/UnitTest1.cs
+++ SeleniumTests/UnitTest1.cs
@@ -13,11 +13,15 @@ namespace SeleniumTests
         [TestMethod]
         public void GoogleSearchTest()
         {
-            var chromeOptions = new ChromeOptions();
-            chromeOptions.BinaryLocation = "/usr/bin/google-chrome-stable";
-            chromeOptions.AddArgument("--headless");
+            //***Configured to work with Travis but may not need this***
+            //var chromeOptions = new ChromeOptions();
+            //chromeOptions.BinaryLocation = "/usr/bin/google-chrome-stable";
+            //chromeOptions.AddArgument("--headless");
+            //IWebDriver driver = new ChromeDriver(chromeOptions);
 
-            IWebDriver driver = new ChromeDriver(chromeOptions);
+            //Original way to get driver and works locally
+            var driverDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            IWebDriver driver = new ChromeDriver(driverDir);
 
             driver.Navigate().GoToUrl("https://www.google.com");
             driver.FindElement(By.Name("q")).SendKeys("How to cook");
@@ -27,28 +31,28 @@ namespace SeleniumTests
             driver.Close();
         }
 
-        [TestMethod]
-        public void TestLogin()
-        {
+        //[TestMethod]
+        //public void TestRegister()
+        //{
 
-            var driverDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            IWebDriver driver = new ChromeDriver(driverDir);
+        //    var driverDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        //    IWebDriver driver = new ChromeDriver(driverDir);
 
-            driver.Navigate().GoToUrl("https://lab03-v2.azurewebsites.net");
-            driver.FindElement(By.XPath("//*[@id='registerLink']")).Click();
-            driver.FindElement(By.XPath("//*[@id='Email']")).SendKeys("qweaasddd@ad.af");
+        //    driver.Navigate().GoToUrl("https://lab03-v2.azurewebsites.net");
+        //    driver.FindElement(By.XPath("//*[@id='registerLink']")).Click();
+        //    driver.FindElement(By.XPath("//*[@id='Email']")).SendKeys("qweaasddd@ad.af");
 
-            driver.FindElement(By.XPath("//*[@id='Password']")).SendKeys("123qwe!@#QWE");
-            driver.FindElement(By.XPath("//*[@id='ConfirmPassword']")).SendKeys("123qwe!@#QWE");
-            driver.FindElement(By.XPath("//*[@value='Register']")).Submit();
+        //    driver.FindElement(By.XPath("//*[@id='Password']")).SendKeys("123qwe!@#QWE");
+        //    driver.FindElement(By.XPath("//*[@id='ConfirmPassword']")).SendKeys("123qwe!@#QWE");
+        //    driver.FindElement(By.XPath("//*[@value='Register']")).Submit();
 
-            var check = driver.FindElement(By.XPath("//*[@title='Manage']"));
+        //    var check = driver.FindElement(By.XPath("//*[@title='Manage']"));
 
-            Assert.AreEqual(check.Text, "Hello qweaasddd@ad.af!");
+        //    Assert.AreEqual(check.Text, "Hello qweaasddd@ad.af!");
 
 
-            driver.Close();
-        }
+        //    driver.Close();
+        //}
 
         [TestMethod]
         public void CounterTest()
