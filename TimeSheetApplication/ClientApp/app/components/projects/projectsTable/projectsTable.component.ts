ba87890414567f47ab639739e29f50d50cf5c568
shelzebub <makaxsole@gmail.com>
2018-03-26 11:42:17 -0700

    added projectsTable and other project related components

---
+++ TimeSheetApplication/ClientApp/app/components/projects/projectsTable/projectsTable.component.ts
@@ -0,0 +1,10 @@
+﻿import { Http, Response } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'projectsTable',
+    styleUrls: ['./projectsTable.component.css'],
+    templateUrl: './projectsTable.component.html'
+})
+export class ProjectsTableComponent { }
