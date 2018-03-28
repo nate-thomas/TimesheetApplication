0d292178b33ce9252dedb43bcbc7d398b213f653
shelzebub <makaxsole@gmail.com>
2018-03-27 17:28:26 -0700

    workpackages

---
+++ TimeSheetApplication/ClientApp/app/components/projects/manageProjects/manageProjects.component.ts
@@ -0,0 +1,10 @@
+﻿import { Http, Response } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'manageProjects',
+    styleUrls: ['./manageProjects.component.css'],
+    templateUrl: './manageProjects.component.html'
+})
+export class ManageProjectsComponent { }
