1b52c5b82e07d41fb4d999c16da6a37845b023d1
kenZino <kennethli126@gmail.com>
2018-04-02 01:07:00 -0700

    test build

--- XUnitTestProject/UnitTests.cs
+++ XUnitTestProject/UnitTests.cs
@@ -12,19 +12,19 @@ namespace XUnitTestProject1
 {
     public class UnitTests
     {
-        [Fact]
-        public void PassTest()
-        {
-            int val1 = 3;
-            int val2 = 45;
+        //[Fact]
+        //public void PassTest()
+        //{
+        //    int val1 = 3;
+        //    int val2 = 45;
 
-            Assert.Equal(48, Add(val1, val2));
-        }
+        //    Assert.Equal(48, Add(val1, val2));
+        //}
 
-        int Add(int num1, int num2)
-        {
-            return num1 + num2;
-        }
+        //int Add(int num1, int num2)
+        //{
+        //    return num1 + num2;
+        //}
 
         [Fact]
         public void GetListOfAllEmployees()
