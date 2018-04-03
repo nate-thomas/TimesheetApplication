1be10fc4f051f32d356e9ee6f4d2c47a2b91d979
kenZino <kennethli126@gmail.com>
2018-04-02 17:50:58 -0700

    Added line to add Moq package to travis YML

--- XUnitTestProject/UnitTest1.cs
+++ XUnitTestProject/UnitTest1.cs
@@ -5,28 +5,19 @@ namespace XUnitTestProject1
 {
     public class UnitTest1
     {
-        [Fact]
-        public void PassTest()
-        {
-            int val1 = 3;
-            int val2 = 45;
+        //[Fact]
+        //public void PassTest()
+        //{
+        //    int val1 = 3;
+        //    int val2 = 45;
 
-            Assert.Equal(48, Add(val1, val2));
-        }
+        //    Assert.Equal(48, Add(val1, val2));
+        //}
 
-//         [Fact]
-//         public void FailTest()
-//         {
-//             int x = 3000;
-//             int y = 6001;
+        //int Add(int num1, int num2)
+        //{
+        //    return num1 + num2;
+        //}
 
-//             Assert.Equal(9000, Add(x, y));
-//         }
-//      Test build
-
-        int Add(int num1, int num2)
-        {
-            return num1 + num2;
-        }
     }
 }
