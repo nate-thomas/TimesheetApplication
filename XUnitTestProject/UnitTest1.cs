ad3829a03fb41db04ad51541561c910e83464c09
kenZino <kennethli126@gmail.com>
2018-04-02 21:55:34 -0700

    added second test sample

--- XUnitTestProject/UnitTest1.cs
+++ XUnitTestProject/UnitTest1.cs
@@ -14,14 +14,23 @@ namespace XUnitTestProject1
             Assert.Equal(48, Add(val1, val2));
         }
 
-//         [Fact]
-//         public void FailTest()
-//         {
-//             int x = 3000;
-//             int y = 6001;
+        [Fact]
+        public void PassTest2()
+        {
+            int val1 = 10;
+            int val2 = 100;
 
-//             Assert.Equal(9000, Add(x, y));
-//         }
+            Assert.Equal(110, Add(val1, val2));
+        }
+
+        //         [Fact]
+        //         public void FailTest()
+        //         {
+        //             int x = 3000;
+        //             int y = 6001;
+
+        //             Assert.Equal(9000, Add(x, y));
+        //         }
 
         int Add(int num1, int num2)
         {
