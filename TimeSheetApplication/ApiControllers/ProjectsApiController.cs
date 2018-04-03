499a60c0f042b42faf97d70554b082040dd293d6
RGollinger <rgollinger@my.bcit.ca>
2018-04-03 13:19:32 -0700

    created blank controller

---
+++ TimeSheetApplication/ApiControllers/ProjectsApiController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TimeSheetApplication.ApiControllers
+{
+    [Produces("application/json")]
+    [Route("api/ProjectsApi")]
+    public class ProjectsApiController : Controller
+    {
+    }
+}
