a4c15bf6fd155f8752d8aeab5a29517dea1f6989
RGollinger <rgollinger@my.bcit.ca>
2018-04-03 14:13:27 -0700

    added endpoints:
    /api/Projects/
    ^^ lists all projects
    /api/WorkPackages/{ProjectNumber}
    ^^ lists all workpackages by project number

--- TimeSheetApplication/ApiControllers/ProjectsApiController.cs
+++ TimeSheetApplication/ApiControllers/ProjectsApiController.cs
@@ -4,12 +4,123 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TimeSheetApplication.Data;
+using TimeSheetApplication.Models.TimeSheetSystem;
 
 namespace TimeSheetApplication.ApiControllers
 {
     [Produces("application/json")]
-    [Route("api/ProjectsApi")]
+    [Route("api/Projects")]
     public class ProjectsApiController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ProjectsApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Projects
+        [HttpGet]
+        public IEnumerable<Project> GetProjects()
+        {
+            return _context.Projects.ToList();
+        }
+
+        // GET: api/ProjectsApi/5
+        [HttpGet("{ProjectNumber}")]
+        public async Task<IActionResult> GetProject([FromRoute] string ProjectNumber)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var project = await _context.Projects.SingleOrDefaultAsync(m => m.ProjectNumber == ProjectNumber);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(project);
+        }
+
+        // PUT: api/ProjectsApi/5
+        [HttpPut("{ProjectNumber}")]
+        public async Task<IActionResult> PutProject([FromRoute] string id, [FromBody] Project project)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != project.ProjectNumber)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(project).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProjectsApi
+        [HttpPost]
+        public async Task<IActionResult> PostProject([FromBody] Project project)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Projects.Add(project);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProject", new { id = project.ProjectNumber }, project);
+        }
+
+        // DELETE: api/ProjectsApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProject([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var project = await _context.Projects.SingleOrDefaultAsync(m => m.ProjectNumber == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            _context.Projects.Remove(project);
+            await _context.SaveChangesAsync();
+
+            return Ok(project);
+        }
+
+        private bool ProjectExists(string id)
+        {
+            return _context.Projects.Any(e => e.ProjectNumber == id);
+        }
     }
 }
