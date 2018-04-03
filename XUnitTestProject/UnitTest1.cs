64007ff9a5d3b8a176861614bb9ec8b4ef2f056e
kenZino <kennethli126@gmail.com>
2018-04-02 18:41:37 -0700

    reverted controller test for troubleshooting

--- XUnitTestProject/UnitTest1.cs
+++ XUnitTestProject/UnitTest1.cs
@@ -11,53 +11,53 @@ namespace XUnitTestProject1
 {
     public class UnitTest1
     {
-        //[Fact]
-        //public void PassTest()
-        //{
-        //    int val1 = 3;
-        //    int val2 = 45;
-
-        //    Assert.Equal(48, Add(val1, val2));
-        //}
-
-        //int Add(int num1, int num2)
-        //{
-        //    return num1 + num2;
-        //}
-
         [Fact]
-        public void GetListOfAllEmployees()
+        public void PassTest()
         {
-            var dbContext = new Mock<IDbContext>();
-            var mockList = MockDbSet(testEmployees);
-            dbContext.Setup(c => c.Employees).Returns(mockList.Object);
+            int val1 = 3;
+            int val2 = 45;
 
-            var controller = new EmployeesApiController(dbContext.Object);
-            var resultList = controller.GetAll().ToList();
-
-            Assert.Equal(5, resultList.Count);
-
+            Assert.Equal(48, Add(val1, val2));
         }
 
-        List<Employee> testEmployees = new List<Employee>()
+        int Add(int num1, int num2)
         {
-            new Employee {EmployeeNumber = "1000021", FirstName = "Ken", LastName = "McCarthy", Grade = "P1", EmployeeIntials = "KEN"},
-            new Employee {EmployeeNumber = "1000022", FirstName = "Ron", LastName = "Swanson", Grade = "P2", EmployeeIntials = "RON"},
-            new Employee {EmployeeNumber = "1000023", FirstName = "Walter", LastName = "White", Grade = "P2", EmployeeIntials = "WW"},
-            new Employee {EmployeeNumber = "1000024", FirstName = "Jesse", LastName = "Pinkman", Grade = "P2", EmployeeIntials = "JP"},
-            new Employee {EmployeeNumber = "1000025", FirstName = "Gus", LastName = "Fring", Grade = "P2", EmployeeIntials = "GF"}
-        };
-
-        Mock<DbSet<T>> MockDbSet<T>(IEnumerable<T> list) where T : class, new()
-        {
-            IQueryable<T> queryableList = list.AsQueryable();
-            Mock<DbSet<T>> dbSetMock = new Mock<DbSet<T>>();
-            dbSetMock.As<IQueryable<T>>().Setup(x => x.Provider).Returns(queryableList.Provider);
-            dbSetMock.As<IQueryable<T>>().Setup(x => x.Expression).Returns(queryableList.Expression);
-            dbSetMock.As<IQueryable<T>>().Setup(x => x.ElementType).Returns(queryableList.ElementType);
-            dbSetMock.As<IQueryable<T>>().Setup(x => x.GetEnumerator()).Returns(() => queryableList.GetEnumerator());
-
-            return dbSetMock;
+            return num1 + num2;
         }
+
+        //[Fact]
+        //public void GetListOfAllEmployees()
+        //{
+        //    var dbContext = new Mock<IDbContext>();
+        //    var mockList = MockDbSet(testEmployees);
+        //    dbContext.Setup(c => c.Employees).Returns(mockList.Object);
+
+        //    var controller = new EmployeesApiController(dbContext.Object);
+        //    var resultList = controller.GetAll().ToList();
+
+        //    Assert.Equal(5, resultList.Count);
+
+        //}
+
+        //List<Employee> testEmployees = new List<Employee>()
+        //{
+        //    new Employee {EmployeeNumber = "1000021", FirstName = "Ken", LastName = "McCarthy", Grade = "P1", EmployeeIntials = "KEN"},
+        //    new Employee {EmployeeNumber = "1000022", FirstName = "Ron", LastName = "Swanson", Grade = "P2", EmployeeIntials = "RON"},
+        //    new Employee {EmployeeNumber = "1000023", FirstName = "Walter", LastName = "White", Grade = "P2", EmployeeIntials = "WW"},
+        //    new Employee {EmployeeNumber = "1000024", FirstName = "Jesse", LastName = "Pinkman", Grade = "P2", EmployeeIntials = "JP"},
+        //    new Employee {EmployeeNumber = "1000025", FirstName = "Gus", LastName = "Fring", Grade = "P2", EmployeeIntials = "GF"}
+        //};
+
+        //Mock<DbSet<T>> MockDbSet<T>(IEnumerable<T> list) where T : class, new()
+        //{
+        //    IQueryable<T> queryableList = list.AsQueryable();
+        //    Mock<DbSet<T>> dbSetMock = new Mock<DbSet<T>>();
+        //    dbSetMock.As<IQueryable<T>>().Setup(x => x.Provider).Returns(queryableList.Provider);
+        //    dbSetMock.As<IQueryable<T>>().Setup(x => x.Expression).Returns(queryableList.Expression);
+        //    dbSetMock.As<IQueryable<T>>().Setup(x => x.ElementType).Returns(queryableList.ElementType);
+        //    dbSetMock.As<IQueryable<T>>().Setup(x => x.GetEnumerator()).Returns(() => queryableList.GetEnumerator());
+
+        //    return dbSetMock;
+        //}
     }
 }
