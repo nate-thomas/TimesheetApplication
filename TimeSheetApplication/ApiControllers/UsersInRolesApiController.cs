82947e34f23ffb7b6de546615187f0271ec8a6f3
waylonching <waylonching@gmail.com>
2018-04-05 17:04:56 -0700

    added basic get users in role feature

---
+++ TimeSheetApplication/ApiControllers/UsersInRolesApiController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNet.Security.OAuth.Validation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TimeSheetApplication.Data;
+using TimeSheetApplication.Models;
+
+namespace TimeSheetApplication.ApiControllers
+{
+    [Produces("application/json")]
+    [Route("api/UsersInRoles")]
+    [Authorize(AuthenticationSchemes = OAuthValidationDefaults.AuthenticationScheme)]
+    [EnableCors("CorsPolicy")]
+    public class UsersInRolesApiController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UsersInRolesApiController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        // GET: api/UsersInRoles/Supervisor
+        [HttpGet("{roleName}")]
+        public async Task<IActionResult> GetUsersInRole([FromRoute] string roleName)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var users = await _userManager.GetUsersInRoleAsync(roleName);
+
+            users.Select()
+
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(users);
+        }
+    }
+}
