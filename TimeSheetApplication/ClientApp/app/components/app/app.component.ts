b0817ba6e78b2016839a9392a3b1627a188484cd
Rei Ruiz <reidominiqueruiz@gmail.com>
2018-04-06 10:42:06 -0700

    Fixing merge conflicts again.

--- TimeSheetApplication/ClientApp/app/components/app/app.component.ts
+++ TimeSheetApplication/ClientApp/app/components/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
     public static url: string = "http://localhost:58911";
-=======
-    public static url: string = "http://localhost:49983";
-
->>>>>>> master
 }
