8c3301a53ccd5a4ad3f5607fb85ee51333d59337
shelzebub <makaxsole@gmail.com>
2018-04-09 17:37:23 -0700

    implemented load projects

--- TimeSheetApplication/ClientApp/app/components/projects/projectsTable/projectsTable.component.ts
+++ TimeSheetApplication/ClientApp/app/components/projects/projectsTable/projectsTable.component.ts
@@ -1,10 +1,72 @@
 ﻿import { Http, Response } from '@angular/http';
 import { Headers, RequestOptions } from '@angular/http';
 import { Component } from '@angular/core';
+import { Project } from '../projects';
+import { Observable } from 'rxjs/Observable';
+import { AppComponent } from '../../app/app.component';
 
 @Component({
     selector: 'projectsTable',
     styleUrls: ['./projectsTable.component.css'],
     templateUrl: './projectsTable.component.html'
 })
-export class ProjectsTableComponent { }
+export class ProjectsTableComponent {
+    project: Project = new Project();
+    projects: Project[] = new Array();
+
+    constructor(private http: Http) { }
+
+
+     /* Functions to be called when component is loaded */
+    
+    ngOnInit() {
+        this.loadProjects();
+    }
+
+    /* Subscription methods to bind the response to a property (if applicable) */
+    loadProjects() {
+        this.getProjects()
+            .subscribe(
+                projects => this.projects = projects
+        );
+
+        console.log('It works here');
+    }
+
+    loadProject(employeeNumber: string) {
+        this.getProject(employeeNumber)
+            .subscribe(
+                project => this.projects = [project]
+        );
+    }
+
+
+    /* CRUD methods to make RESTful calls to the API */
+
+    getProjects(): Observable<Project[]> {
+        let headers = new Headers({ 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + localStorage.getItem('access_token') })
+        let options = new RequestOptions({ headers: headers });
+
+        console.log('It works here3');
+
+        return this.http.get(AppComponent.url + "/api/Projects/", options)
+            .map((res: Response) => res.json())
+            .catch((err: Response) => {
+                console.log(JSON.stringify(err));
+                return Observable.throw(new Error(JSON.stringify(err)));
+            });   
+    }
+
+    getProject(employeeNumber: string): Observable<Project> {
+        let headers = new Headers({ 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + localStorage.getItem('access_token') })
+        let options = new RequestOptions({ headers: headers });
+
+        return this.http.get(AppComponent.url + "/api/Projects/" + employeeNumber, options)
+            .map((res: Response) => res.json())
+            .catch((err: Response) => {
+                console.log(JSON.stringify(err));
+                return Observable.throw(new Error(JSON.stringify(err)));
+            });
+    }
+
+}
---
+++ TimeSheetApplication/Migrations/20180410003446_Initial.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TimeSheetApplication.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProjectManager",
+                table: "Projects",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_ProjectManager",
+                table: "Projects",
+                column: "ProjectManager");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Employees_ProjectManager",
+                table: "Projects",
+                column: "ProjectManager",
+                principalTable: "Employees",
+                principalColumn: "EmployeeNumber",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Employees_ProjectManager",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_ProjectManager",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectManager",
+                table: "Projects");
+        }
+    }
+}
